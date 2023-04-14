fc89a0ff3f7f261f9f2f06f919581f4d5d36acc3
thiagorazzini <razzini.thiago@gmail.com>
2023-04-14 14:16:37 -0300

    Using methods and atributes to calculate qtd and prices

---
+++ Product/Product/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Product
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+
+
+        public void AddProducts(int quantity)
+        {
+
+            Quantity += quantity;
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " unidades, Total: $"
+                + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Product/Product/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+namespace Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            
+            
+            Console.WriteLine("Entre os dados do Produto");
+            Console.Write("Nome: ");
+
+            product.Name = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            product.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            product.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {product}");
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int addproducts = int.Parse( Console.ReadLine());
+            product.AddProducts(addproducts);
+
+            Console.WriteLine($"Dados atualizados: {product}");
+
+
+
+
+            Console.Write("Digite o número de produtos a ser removidos do estoque: ");
+            int removeProducts = int.Parse(Console.ReadLine());
+            product.RemoveProducts(removeProducts);
+
+            Console.Write($"Dados atualizados: {product}");
+
+
+        }
+    }
+}
