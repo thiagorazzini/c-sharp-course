fd52a7c062d80bad0d8ce5edfac114fa3802041e
thiagorazzini <razzini.thiago@gmail.com>
2023-04-14 14:38:24 -0300

    Solve retangle problem

---
+++ Retangle-Exercise/Retangle-Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Retangle_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangle retangle = new Retangle();
+
+            Console.WriteLine("Entre a largura e altura do retângulo");
+            retangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangle.Height = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangle);
+        }
+    }
+}
---
+++ Retangle-Exercise/Retangle-Exercise/Retangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangle_Exercise
+{
+    internal class Retangle
+    {
+        public double Width;
+        public double Height;
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+
+        public double Perimeter()
+        {
+            return 2* Width + 2* Height;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = "
+                + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "PERÍMETRO = "
+                + Perimeter().ToString("F2", CultureInfo.InvariantCulture)
+                + "\n"
+                + "DIAGONAL = "
+                + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
