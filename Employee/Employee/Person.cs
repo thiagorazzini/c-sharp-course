a60c62f24c339b9ce02d8d9ec8c5f096c08631ca
thiagorazzini <razzini.thiago@gmail.com>
2023-04-16 19:16:32 -0300

    Done exercise of Employee

---
+++ Employee/Employee/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Employee
+{
+    internal class Person
+    {
+        public string Name;
+        public double Salary;
+        public double Taxes;
+        public double GrowthSalary;
+
+
+        public void calcSalary()
+        {
+            Salary -=  Taxes;
+        }
+
+        public void growthSalaryCalc()
+        {
+            GrowthSalary = GrowthSalary / 100;
+
+            Salary += ((Salary + Taxes) * GrowthSalary);
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            p.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            p.Taxes = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            p.calcSalary();
+
+            Console.WriteLine($"Funcionário: {p}");
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            p.GrowthSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            p.growthSalaryCalc();
+
+
+            Console.WriteLine($"Dados atualizados: {p}");
+        }
+    }
+}
