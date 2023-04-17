988d134b7454a74c4249b9401d02499b0cea76cf
thiagorazzini <razzini.thiago@gmail.com>
2023-04-17 14:32:24 -0300

    Created a new static class for calculator

---
+++ StaticMembers/StaticMembers/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace StaticMembers
+{
+
+    internal class Calculator
+    {
+        public static double Pi = 3.14;
+
+
+        public static double Circumference(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
--- StaticMembers/StaticMembers/Program.cs
+++ StaticMembers/StaticMembers/Program.cs
@@ -3,28 +3,20 @@ namespace StaticMembers
 {
     internal class Program
     {
-        static double pi = 3.14;
         static void Main(string[] args)
         {
+
             Console.WriteLine("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circumference(raio);
-            double volume = Volume(raio);
+            double circ = Calculator.Circumference(raio);
+            double volume = Calculator.Volume(raio);
 
             Console.WriteLine("Circunferência:" + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
 
-        static double Circumference(double r)
-        {
-            return 2.0 * pi * r;
-        }
 
-        static double Volume( double r)
-        {
-            return 4.0 / 3.0 * pi * Math.Pow(r, 3.0);
-        }
     }
 }
