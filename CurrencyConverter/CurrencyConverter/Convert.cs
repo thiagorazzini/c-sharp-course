2fe12bba3727e7fa2af6780ebbeee8f5a328678b
thiagorazzini <razzini.thiago@gmail.com>
2023-04-17 14:54:15 -0300

    Done challange of convert currency

---
+++ CurrencyConverter/CurrencyConverter/Convert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    internal class Convert
+    {
+        public static double Iof = 0.06;
+
+        public static double CalcValue(double valueToBuy, double quantity)
+        {
+            double taxes = (valueToBuy * quantity) * Iof;
+            double totalValue = (valueToBuy * quantity) + taxes;
+
+            return totalValue;  
+        }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollar, quantity;
+            Console.Write("Qual é a cotação do dólar ?");
+            dollar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            quantity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor pago em reais = {Convert.CalcValue(quantity, dollar).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
