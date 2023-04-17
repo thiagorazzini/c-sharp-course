5c56ad26ec38c664cd0e829a5bb597894295bf61
thiagorazzini <razzini.thiago@gmail.com>
2023-04-17 14:09:22 -0300

    Add all solutions

---
+++ StudentAverage/StudentAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+namespace StudentAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudantData student = new StudantData();
+
+            Console.Write("Nome do aluno: ");
+            student.Name = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            student.GradesOne = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.GradesTwo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.GradesThree = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NOTA FINAL = {student.gradesSum().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(student.verifySituation());
+            
+        }
+    }
+}
---
+++ StudentAverage/StudentAverage/StudantData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace StudentAverage
+{
+    internal class StudantData
+    {
+        public string Name;
+        public double GradesOne;
+        public double GradesTwo;
+        public double GradesThree;
+
+
+        public double gradesSum()
+        {
+            return GradesOne + GradesTwo + GradesThree;
+        }
+
+        public string verifySituation()
+        {
+            if(GradesOne + GradesTwo + GradesThree > 60.0)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                double needPoint = 60.0 - (GradesOne + GradesTwo + GradesThree);
+                return $"REPROVADO \nFALTARAM {needPoint.ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+            }
+            
+        }
+    }
+}
