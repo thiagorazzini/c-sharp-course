7eb7845f0d4f15d3091623c99a070b91713c57fe
thiagorazzini <razzini.thiago@gmail.com>
2023-04-18 16:11:34 -0300

    Lesson about constructors

---
+++ ConstructorLesson/ConstructorLesson/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Product
+{
+    internal class ProductNam
+    {
+        private string _name;
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+
+
+
+
+        public ProductNam() { }
+
+        public ProductNam(string name, double price, int quantity) {
+            _name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name {
+            get { return _name; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+
+
+        public void AddProducts(int quantity)
+        {
+
+            Quantity += quantity;
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return _name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " unidades, Total: $"
+                + TotalValue().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ConstructorLesson/ConstructorLesson/Program.cs
@@ -0,0 +1,21 @@
+﻿using Product;
+using System.Globalization;
+
+namespace ConstructorLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductNam p = new ProductNam("TV", 500.00, 10);
+
+            p.Name = "T";
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Price);
+            Console.WriteLine(p.Quantity);
+
+        }
+    }
+}
