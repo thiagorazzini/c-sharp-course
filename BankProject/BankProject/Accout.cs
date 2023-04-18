04c693b3539746cc0122c6c3326435e74e0ee7a5
thiagorazzini <razzini.thiago@gmail.com>
2023-04-18 16:41:49 -0300

    New project using object-oriented Programming Concepts

---
+++ BankProject/BankProject/Accout.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    internal class Accout
+    {
+        public string Name { get; private set; }
+        public int NumberAccount { get; private set; }
+        public double Balance { get; private set; }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public static bool VerifyInicialValue(string confirm)
+        {
+            if (confirm.Contains('s'))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+
+
+
+    }
+}
---
+++ BankProject/BankProject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            
+            Accout accout = new Accout();
+            accout.SetName = Console.ReadLine();
+
+        }
+    }
+}
