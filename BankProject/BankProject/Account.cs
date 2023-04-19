ee3453d0d8b98e907bbc78503902336da02a08e4
thiagorazzini <razzini.thiago@gmail.com>
2023-04-19 16:30:17 -0300

    Create all parts and started  POO concepts

---
+++ BankProject/BankProject/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    internal class Account
+    {
+        private string _nameOwner;
+        public int AccountNumber  { get; private set; }
+        private double _balance;
+
+
+
+        public Account(int accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public string NameOwner
+        {
+            get { return _nameOwner; }
+            set { _nameOwner = value; }
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+
+        public void Verify(char isDeposite)
+        {
+            if(isDeposite == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Balance += value;
+            }
+        }
+
+        public void Deposit()
+        {
+            Console.Write("Entre um valor para deposito: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Balance += value;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                   + AccountNumber
+                   + ", Titular: "
+                   + NameOwner
+                   + ", Saldo: $"
+                   + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- BankProject/BankProject/Program.cs
+++ BankProject/BankProject/Program.cs
@@ -1,13 +1,56 @@
-﻿namespace BankProject
+﻿using System.Globalization;
+
+namespace BankProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double value;
+
+            char verify;
+            double balance;
+
+
             Console.Write("Entre o número da conta: ");
+            int account = int.Parse(Console.ReadLine());
+
+            Account p = new Account(account);
+
+
             
-            Accout accout = new Accout();
-            accout.SetName = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            p.NameOwner = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n) ? ");
+            verify = char.Parse(Console.ReadLine());
+
+            p.Verify(verify);
+
+
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(p);
+
+            p.Deposit();
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(p);
+            //Console.WriteLine($"Conta {account}, Titular: {nameOwner}, Saldo: ${balance.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+            //Console.Write("Entre um valor para depósito: ");
+            //value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //balance += value;
+            //Console.WriteLine("Dados da conta atualizados: ");
+            //Console.WriteLine($"Conta {account}, Titular: {nameOwner}, Saldo: ${balance.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            //Console.Write("Entre um valor para saque: ");
+            //double withdrawal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //balance -= withdrawal + 5.00;
+
+            //Console.WriteLine("Dados da conta atualizados: ");
+            //Console.WriteLine($"Conta {account}, Titular: {nameOwner}, Saldo: ${balance.ToString("F2", CultureInfo.InvariantCulture)}");
+
 
         }
     }
