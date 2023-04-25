42852a6a86524627dc49da8ab914c7676edc63c9
thiagorazzini <razzini.thiago@gmail.com>
2023-04-25 13:01:09 -0300

    Params studies

---
+++ ParamsCalculator/ParamsCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ParamsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3 );
+            int s2 = Calculator.Sum(2, 4, 3);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
