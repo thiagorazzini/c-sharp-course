0e6a0b82c8d25103c665661579d8a79b0cab3bc2
thiagorazzini <razzini.thiago@gmail.com>
2023-04-25 14:48:08 -0300

    Exercise about lists

---
+++ EmployeeList/EmployeeList/EmployeeRegister.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    internal class EmployeeRegister
+    {
+
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public double Salary { get; private set; }
+
+        public EmployeeRegister(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            Salary *= (percentage / 100);
+        }
+    }
+}
---
+++ EmployeeList/EmployeeList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered ? ");
+            int employeeToRegister = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>(employeeToRegister);
+
+            
+
+            for(int i = 1; i <= employeeToRegister; i++)
+            {
+                Console.WriteLine($"Employee #{i}");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+            }
+
+
+
+
+
+        }
+    }
+}
