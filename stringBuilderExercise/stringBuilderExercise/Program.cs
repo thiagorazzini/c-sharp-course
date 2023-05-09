43476381205866e0174e4a243531003280137741
thiagorazzini <razzini.thiago@gmail.com>
2023-05-09 14:25:30 -0300

    StringBuilder

--- stringBuilderExercise/stringBuilderExercise/Program.cs
+++ stringBuilderExercise/stringBuilderExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace stringBuilderExercise
+﻿using stringBuilderExercise.Entities;
+
+namespace stringBuilderExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome !");
+            Post p1 = new Post(
+                   DateTime.Parse("21/06/2018 13:05:44"),
+                   "Traveling to New Zealand",
+                   "I'm going to visit this wonderful country",
+                   12
+                );
+
+            p1.Comments.Add(c1);
+            p1.Comments.Add(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                       DateTime.Parse("28/07/2018 23:25:44"),
+                       "Good night guys",
+                       "See you tomorrow",
+                       5
+                    );
+            p2.Comments.Add(c3);
+            p2.Comments.Add(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
         }
     }
 }
