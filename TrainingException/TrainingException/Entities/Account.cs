5c89534b8c962413ce4a5eac95de67164611f432
thiagorazzini <razzini.thiago@gmail.com>
2023-05-19 18:20:55 -0300

    Finished implemetation of exception exercise

---
+++ TrainingException/TrainingException/Entities/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingException.Entities.Exception;
+using System.Globalization;
+
+namespace TrainingException.Entities
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+            
+        }
+
+        public Account(int accountNumber, string holder, double balance, double withDrawLimit)
+        {
+            AccountNumber = accountNumber;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if(Balance < amount)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            if(amount > WithDrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            Balance -= amount;
+
+        }
+
+        public override string ToString()
+        {
+            return "New Balance: "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ TrainingException/TrainingException/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using TrainingException.Entities;
+using TrainingException.Entities.Exception;
+
+namespace TrainingException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int accountNumber = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string accountHolder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(accountNumber, accountHolder, initialBalance, withDrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double withdrawValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.WithDraw(withdrawValue);
+                Console.WriteLine(account);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+        }
+    }
+}
