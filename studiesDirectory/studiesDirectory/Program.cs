331df427c30563dd94ac2dfbbc5442f07e74d540
thiagorazzini <razzini.thiago@gmail.com>
2023-05-29 15:41:50 -0300

    understand directory

---
+++ studiesDirectory/studiesDirectory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace studiesDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+
+            try
+            {
+                var folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string item in folder)
+                {
+                    Console.WriteLine(item);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+                foreach (string item in files)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Directory.CreateDirectory(path + @"\newFolder");
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
