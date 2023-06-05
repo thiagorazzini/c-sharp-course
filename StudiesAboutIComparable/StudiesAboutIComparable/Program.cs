4b43365ae4adb70057fb32c6269ccedc5fadf1f5
thiagorazzini <razzini.thiago@gmail.com>
2023-06-04 22:10:57 -0300

    Explain about compareto

---
+++ StudiesAboutIComparable/StudiesAboutIComparable/Program.cs
@@ -0,0 +1,33 @@
+﻿using StudiesAboutIComparable.Entities;
+
+namespace StudiesAboutIComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }catch (IOException ex)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
