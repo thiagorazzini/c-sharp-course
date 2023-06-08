0dbba0d19a22c93af9a3ed1b6a215d13d230b059
thiagorazzini <razzini.thiago@gmail.com>
2023-06-08 15:15:16 -0300

    Cosolidation dictionary

---
+++ dictionaryConsolidation/dictionaryConsolidation/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace dictionaryConsolidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter file full path: ");
+            string fullPath = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(fullPath))
+                {
+                    Dictionary<string, int> candidates = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string candidateName = line[0];
+                        int vote = int.Parse(line[1]);
+
+                        if (candidates.ContainsKey(candidateName))
+                        {
+                            candidates[candidateName] += vote;
+                        }
+                        else
+                        {
+                            candidates[candidateName] = vote;
+                        }
+
+
+                    }
+                    foreach (var item in candidates)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred during the execution");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
