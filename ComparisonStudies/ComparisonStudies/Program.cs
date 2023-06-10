6f6e9c1f95e4d69cabf29d971e0364a07a59bd23
thiagorazzini <razzini.thiago@gmail.com>
2023-06-10 19:55:54 -0300

    Studies about IComparison

---
+++ ComparisonStudies/ComparisonStudies/Program.cs
@@ -0,0 +1,24 @@
+﻿using ComparisonStudies.Entities;
+
+namespace ComparisonStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+
+            products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product product in products) {
+                Console.WriteLine(product);
+            }
+        }
+
+    }
+}
---
+++ extentionMethods/extentionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using extentionMethods.Extentions;
+
+namespace extentionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+
+        }
+    }
+}
