35c75197fe75cf4b2f21087988faea6774e4f11e
thiagorazzini <razzini.thiago@gmail.com>
2023-06-11 19:25:24 -0300

    Predicates

---
+++ PredicatesStudies/PredicatesStudies/Program.cs
@@ -0,0 +1,29 @@
+﻿using PredicatesStudies.Entities;
+
+namespace PredicatesStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Mouse", 50.00));
+            products.Add(new Product("Tablet", 350.00));
+            products.Add(new Product("HD Case", 80.00));
+
+            products.RemoveAll(ProductTest);
+            foreach(Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100;
+        }
+    }
+}
