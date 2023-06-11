daffaac40d6ad0ce46e15e489525af54fb61190f
thiagorazzini <razzini.thiago@gmail.com>
2023-06-11 19:05:00 -0300

    delegate lesson

---
+++ DelegatesStudies/DelegatesStudies/Program.cs
@@ -0,0 +1,20 @@
+﻿using DelegatesStudies.Services;
+
+namespace DelegatesStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+
+
+            double result  = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ DelegatesStudies/DelegatesStudies/Services/CalculationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesStudies.Services
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    internal class CalculationService
+    {
+
+        BinaryNumericOperation op = CalculationService.Sum;
+        public static double Max(double x,  double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x, double y)
+        {
+            return x * x;
+        }
+    }
+}
