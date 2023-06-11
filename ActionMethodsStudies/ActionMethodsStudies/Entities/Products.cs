13bca184e3f6d8136f004fe25eb4e44859506d89
thiagorazzini <razzini.thiago@gmail.com>
2023-06-11 19:34:43 -0300

    Work with actions

---
+++ ActionMethodsStudies/ActionMethodsStudies/Entities/Products.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ActionMethodsStudies.Entities
+{
+    internal class Products
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() => Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
---
+++ ActionMethodsStudies/ActionMethodsStudies/Program.cs
@@ -0,0 +1,33 @@
+﻿using ActionMethodsStudies.Entities;
+
+namespace ActionMethodsStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> products = new List<Products>();
+
+            products.Add(new Products("Tv", 900.00));
+            products.Add(new Products("Mouse", 50.00));
+            products.Add(new Products("Tablet", 350.50));
+            products.Add(new Products("HD Case", 80.90));
+
+            //Action<Products> act = p => { p.Price += p.Price * 0.1; };
+
+            products.ForEach(p => { p.Price += p.Price * 0.1; };);
+
+            foreach (var product in products) {
+                Console.WriteLine(product);
+            }
+            
+
+
+        }
+
+        static void UpdatePrice(Products products)
+        {
+            products.Price += products.Price * 0.1;
+        }
+    }
+}
