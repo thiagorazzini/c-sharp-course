f68271fcbbac3f4d8dcb9a1b2ab9b5fae9f07b93
thiagorazzini <razzini.thiago@gmail.com>
2023-06-11 19:48:03 -0300

    Using func lambda

---
+++ FuncStudies/FuncStudies/Program.cs
@@ -0,0 +1,29 @@
+﻿using FuncStudies.Entities;
+using System.Linq;
+
+namespace FuncStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Mouse", 50.00));
+            products.Add(new Product("Tablet", 350.00));
+            products.Add(new Product("HD Case", 80.90));
+            //Func<Product, string> func = p => p.Name.ToUpper();
+
+
+            List<string> result = products.Select(p => p.Name.ToUpper()).ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
