8df97407e6655eebcf949863a75969f55ed937a5
thiagorazzini <razzini.thiago@gmail.com>
2023-06-13 13:11:43 -0300

    Challange of linq employee code

---
+++ CSVReadAveragePrice/CSVReadAveragePrice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using CSVReadAveragePrice.Entities;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace CSVReadAveragePrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> products = new List<Product>();
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] prod = sr.ReadLine().Split(',');
+                    string productName = prod[0];
+                    double price = double.Parse(prod[1], CultureInfo.InvariantCulture);
+                    products.Add(new Product(productName, price));
+
+                }
+            }
+
+            var avg = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+
+            Console.WriteLine("Average price " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p=> p.Name);
+            
+            foreach(var p in names)
+            {
+                Console.WriteLine(p);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ SalaryAverage/SalaryAverage/Program.cs
@@ -0,0 +1,45 @@
+﻿using SalaryAverage.Entities;
+using System.Globalization;
+using System.IO;
+
+namespace SalaryAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] employee = sr.ReadLine().Split(',');
+                    string employeeName = employee[0];
+                    string employeeEmail = employee[1];
+                    double employeePrice = double.Parse(employee[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(employeeName, employeeEmail, employeePrice));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double salaryAvg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Email of people whose salary is more than: " + salaryAvg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var avg = employees.Where(p => p.Salary > salaryAvg).OrderBy(p => p.Email).Select(p => p.Email);
+            var salarySum = employees.Where(p => p.Name.StartsWith('M')).Sum(p => p.Salary);
+
+            foreach(var em in avg)
+            {
+                Console.WriteLine(em);
+            }
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + salarySum.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
