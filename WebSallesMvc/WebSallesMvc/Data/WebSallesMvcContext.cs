983f5295b1dea46572e4d0bb5e319389d1c63d0f
thiagorazzini <razzini.thiago@gmail.com>
2023-06-15 06:35:41 -0300

    CRUD Scaffolding

---
+++ WebSallesMvc/WebSallesMvc/Data/WebSallesMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSallesMvc.Models;
+
+namespace WebSallesMvc.Data
+{
+    public class WebSallesMvcContext : DbContext
+    {
+        public WebSallesMvcContext (DbContextOptions<WebSallesMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSallesMvc.Models.Departament> Departament { get; set; } = default!;
+    }
+}
--- WebSallesMvc/WebSallesMvc/Program.cs
+++ WebSallesMvc/WebSallesMvc/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebSallesMvc.Data;
 namespace WebSallesMvc
 {
     public class Program
@@ -5,6 +8,8 @@ namespace WebSallesMvc
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<WebSallesMvcContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("WebSallesMvcContext") ?? throw new InvalidOperationException("Connection string 'WebSallesMvcContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
